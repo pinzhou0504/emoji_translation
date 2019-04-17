14081b4b73e06bb736617bef8904aeda3658c3c6
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-04-17 16:19:52 -0400

    push user columns to GitHub

---
+++ src/main/resources/db/migration/V1.2__Alter_User_Insert_Column.sql
@@ -0,0 +1,4 @@
+Alter table users add column first_name varchar(255);
+Alter table users add column last_name varchar(255);
+Alter table users add column email varchar(255);
+Alter table users add column password varchar(255);
---
+++ src/main/resources/db/migration/V1.3__Alter_User_Alter_Columns.sql
@@ -0,0 +1,8 @@
+Alter table users alter column last_name set default 'Smith';
+Alter table users alter column email set default '1234567@email.com';
+Alter table users alter column password set default '123456';
+
+
+Alter table users alter column last_name set not null;
+Alter table users alter column last_name set not null;
+Alter table users alter column last_name set not null;
