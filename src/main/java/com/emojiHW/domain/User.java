d596f04d74603cf973a324ea1d24ac9f17c41040
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-04-17 16:17:41 -0400

    add User.java

--- src/main/java/com/emojiHW/domain/User.java
+++ src/main/java/com/emojiHW/domain/User.java
@@ -19,4 +19,15 @@ public class User {
 
     @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+
 }
