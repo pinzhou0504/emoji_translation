ba365cd8d22e1fdb8b7061e085feb828590f231a
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-04-17 16:26:40 -0400

    add emoji table

---
+++ src/main/resources/db/migration/V1.4__Emoji_Table.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE emojis_id_seq;
+create table Emoji (
+    id bigint not null DEFAULT NEXTVAL('emojis_id_seq'),
+    primary key (id),
+    code varchar(255),
+    cldr_short_name varchar(255)
+);
+ALTER SEQUENCE emojis_id_seq OWNED BY Emoji.id;
