e683308ab5f363986eb2c99932f8a15699de239c
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-04-19 17:22:17 -0400

    add conversations and username_id column

--- src/main/java/com/emojiHW/domain/Conversation.java
+++ src/main/java/com/emojiHW/domain/Conversation.java
@@ -1,10 +1,16 @@
 package com.emojiHW.domain;
 
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 public class Conversation {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "conversations_id_seq")
+    @SequenceGenerator(name = "conversations_id_seq",sequenceName = "conversations_id_seq",allocationSize = 1)
+
+    private Long conversations;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "username_id")
     private User username;
--- src/main/resources/db/migration/V1.6__Conversation_Table.sql
+++ src/main/resources/db/migration/V1.6__Conversation_Table.sql
@@ -1,6 +1,7 @@
 CREATE SEQUENCE username_id_seq;
 CREATE table Conversation(
- username_id bigint not null DEFAULT NEXTVAL('username_id_seq'),
- primary key (username_id)
+    conversations bigint not null DEFAULT NEXTVAL('conversations_id_seq'),
+    primary key (conversations),
+    username_id varchar (255)
 ) ;
 ALTER SEQUENCE username_id_seq OWNED BY Conversation.id;
