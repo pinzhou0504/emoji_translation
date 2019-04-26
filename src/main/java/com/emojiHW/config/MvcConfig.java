86574a430dd2beed220875c9d16a72763a4e90f1
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-04-26 17:22:39 -0400

    modify MvcConfig

--- src/main/java/com/emojiHW/config/MvcConfig.java
+++ src/main/java/com/emojiHW/config/MvcConfig.java
@@ -2,6 +2,9 @@ package com.emojiHW.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,4 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan("com.emojiHW.api")
 public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+
 }
