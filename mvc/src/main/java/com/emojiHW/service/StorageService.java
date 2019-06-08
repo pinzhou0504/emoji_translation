46a06180f59ba4c7fa2028faeeb3a14c48f23305
Pinzhou Liang <Pinzhou@Pinzhous-MacBook-Pro.local>
2019-06-08 12:40:22 -0400

    add mvc and worker and spring boot

--- mvc/src/main/java/com/emojiHW/service/StorageService.java
+++ mvc/src/main/java/com/emojiHW/service/StorageService.java
@@ -2,22 +2,15 @@ package com.emojiHW.service;
 
 import java.io.File;
 import java.net.URL;
-import java.security.Key;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StorageService {
     private AmazonS3 s3Client;
-
     //            = AmazonS3ClientBuilder.standard()
 //            .withRegion(clientRegion)
 //            .withCredentials(new DefaultAWSCredentialsProviderChain())
@@ -26,10 +19,7 @@ public class StorageService {
             this.s3Client=s3;}
 
     public void uploadObject(String bucketName, File file) {
-
 //        try {
-
-
         // Upload a file as a new object with ContentType and title specified.
         PutObjectRequest request = new PutObjectRequest(bucketName, file.getName(), file);
         s3Client.putObject(request);
@@ -48,7 +38,6 @@ public class StorageService {
 
     public String getObjectUrl(String bucketName, String key) {
 //        S3Object fullObject = null;
-
 //        fullObject = s3Client.getObject(new GetObjectRequest(bucketName, key));
         URL url = s3Client.getUrl(bucketName, key);
         String urlStr = url.toString();
